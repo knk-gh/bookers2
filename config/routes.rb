046269fd1f08683dcd5875ec383a855bad7d8c56
knk <knk@knknoMacBook-puro.local>
2018-09-17 13:10:34 +0900

    [add]途中その１

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  authenticated :user do
+  	root :to => "users#show", :as => "user_authenticated_root"
+  end
+  root 'home#top'
+
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+
+
+
+  get '/about' => 'home#about'
+  get '/users/sign_in' => 'sessions#new'
+  resources :users, only: [:show]
 end
